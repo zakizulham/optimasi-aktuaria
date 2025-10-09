cfc438cda3b46367a08abd1d71b0fffa733fe901
zakizulham <zakizulham205@gmail.com>
2025-10-09 20:59:27 +0700

    updated main.py dengan soal mengikuti tugas 1 nomor 1

--- OptiManufaktur/src/main.py
+++ OptiManufaktur/src/main.py
@@ -7,12 +7,15 @@ def main():
     """
     print("--- OptiManufaktur Solver ---")
 
-    # Definisikan masalah dari Fase 1
+    np.set_printoptions(linewidth=np.inf, suppress=True, precision=3) # type: ignore
+
+    # Definisikan masalah baru
     # Kolom: [x1, x2, s1, s2, Z, Solusi]
     tableau_phase1 = np.array([
-        [4,   2,  1,  0,  0,  60],
-        [2,   4,  0,  1,  0,  48],
-        [-80, -60, 0,  0,  1,  0]
+        [2,   1,  1,  0,  0,  50 ],
+        [2,   5,  0,  1,  0,  100],
+        [2,   3,  1,  0,  0,  90 ],
+        [-4, -10, 0,  0,  1,  0  ]
     ])
 
     # Inisialisasi solver dengan masalah kita
@@ -20,6 +23,7 @@ def main():
 
     # Cetak tabel awal untuk verifikasi
     print("Tabel Simpleks Awal (Fase 1):")
+    print("Ingat Kolom: [x1, x2, s1, s2, Z, Solusi]")
     print(solver)
 
     # Panggil method solve (saat ini belum melakukan apa-apa)
