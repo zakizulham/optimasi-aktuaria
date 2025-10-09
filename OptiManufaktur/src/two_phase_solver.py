55f1786d6441b264f0d3eb6cc95dd1c3fc5d7ab8
zakizulham <zakizulham205@gmail.com>
2025-10-09 21:14:32 +0700

    added method phase1 tableau at class two phases class

--- OptiManufaktur/src/two_phase_solver.py
+++ OptiManufaktur/src/two_phase_solver.py
@@ -13,3 +13,28 @@ class TwoPhaseSimplexSolver:
         self.original_c = c
         self.A = A_eq
         self.b = b_eq
+
+    def _build_phase1_tableau(self):
+        """Membangun tableau awal untuk Fase 1."""
+        num_constraints, num_vars = self.A.shape
+        
+        # Buat matriks artifisial (matriks identitas)
+        A_artificial = np.identity(num_constraints)
+        
+        # Gabungkan matriks A dengan matriks artifisial
+        # Kolom: [vars_asli, vars_artifisial, Solusi]
+        A_phase1 = np.hstack([self.A, A_artificial, self.b.reshape(-1, 1)])
+        
+        # Buat baris tujuan Fase 1: Minimalkan W = sum(a_i) -> Maksimalkan R = -sum(a_i)
+        # R + a1 + a2 + ... = 0
+        c_phase1 = np.zeros(A_phase1.shape[1])
+        c_phase1[num_vars : num_vars + num_constraints] = 1 # Koefisien a_i di R
+        c_phase1[-2] = 1 # Koefisien R
+
+        tableau = np.vstack([A_phase1, c_phase1])
+        
+        # Jadikan tableau kanonik: koefisien var basis (a_i) di baris R harus 0
+        for i in range(num_constraints):
+            tableau[-1,:] -= tableau[i,:]
+            
+        return tableau
