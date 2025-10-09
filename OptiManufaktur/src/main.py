a52c2ad6f3e57e310ebdafd05dd16e90f8d90c31
zakizulham <zakizulham205@gmail.com>
2025-10-09 22:07:49 +0700

    updated with num_decision_vars parameter

--- OptiManufaktur/src/main.py
+++ OptiManufaktur/src/main.py
@@ -10,20 +10,20 @@ def main():
     np.set_printoptions(linewidth=np.inf, suppress=True, precision=3) # type: ignore
 
     # Definisikan masalah baru
-    # Kolom: [x1, x2, s1, s2, Z, Solusi]
+    # Kolom: [x1, x2, s1, s2, s3, Z, Solusi]
     tableau_phase1 = np.array([
-        [2,   1,  1,  0,  0,  50 ],
-        [2,   5,  0,  1,  0,  100],
-        [2,   3,  1,  0,  0,  90 ],
-        [-4, -10, 0,  0,  1,  0  ]
+        [2,   1,  1,  0,  0,  0,  50 ],
+        [2,   5,  0,  1,  0,  0,  100],
+        [2,   3,  0,  0,  1,  0,  90 ],
+        [-4, -10, 0,  0,  0,  1,  0  ]
     ])
 
     # Inisialisasi solver dengan masalah kita
-    solver = SimplexSolver(tableau_phase1)
+    solver = SimplexSolver(tableau_phase1, num_decision_vars = 2)
 
     # Cetak tabel awal untuk verifikasi
     print("Tabel Simpleks Awal (Fase 1):")
-    print("Ingat Kolom: [x1, x2, s1, s2, Z, Solusi]")
+    print("Ingat Kolom: [x1, x2, s1, s2, s3, Z, Solusi]")
     print(solver)
 
     # Panggil method solve (saat ini belum melakukan apa-apa)
